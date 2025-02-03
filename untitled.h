c3add7208e0db85d657c4bdd3441b476a6a12b87
unknown <thanhnq.10012k1@gmail.com>
2025-02-04 02:01:51 +0700

    e2e

---
+++ untitled.h
@@ -0,0 +1,55 @@
+/*
+ * File: untitled.h
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 9.9 (R2023a) 19-Nov-2022
+ * C/C++ source code generated on : Tue Feb  4 01:26:32 2025
+ *
+ * Target selection: autosar.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_untitled_h_
+#define RTW_HEADER_untitled_h_
+#ifndef untitled_COMMON_INCLUDES_
+#define untitled_COMMON_INCLUDES_
+#include "rtwtypes.h"
+#include "Rte_E2E.h"
+#endif                                 /* untitled_COMMON_INCLUDES_ */
+
+#include "untitled_types.h"
+
+/*-
+ * The generated code includes comments that allow you to trace directly
+ * back to the appropriate location in the model.  The basic format
+ * is <system>/block_name, where system is the system number (uniquely
+ * assigned by Simulink) and block_name is the name of the block.
+ *
+ * Use the MATLAB hilite_system command to trace the generated code back
+ * to the model.  For example,
+ *
+ * hilite_system('<S3>')    - opens system 3
+ * hilite_system('<S3>/Kp') - opens and selects block Kp which resides in S3
+ *
+ * Here is the system hierarchy for this model
+ *
+ * '<Root>' : 'untitled'
+ * '<S1>'   : 'untitled/Runnable_10ms'
+ * '<S2>'   : 'untitled/Runnable_Initialize'
+ */
+
+/*-
+ * Requirements for '<Root>': untitled
+
+ */
+#endif                                 /* RTW_HEADER_untitled_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ untitled_private.h
@@ -0,0 +1,26 @@
+/*
+ * File: untitled_private.h
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 9.9 (R2023a) 19-Nov-2022
+ * C/C++ source code generated on : Tue Feb  4 01:26:32 2025
+ *
+ * Target selection: autosar.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_untitled_private_h_
+#define RTW_HEADER_untitled_private_h_
+#include "rtwtypes.h"
+#include "untitled_types.h"
+#endif                                 /* RTW_HEADER_untitled_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */

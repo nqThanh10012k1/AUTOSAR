3a46e638235e59495f66354a629e7800b4f3e746
unknown <thanhnq.10012k1@gmail.com>
2025-05-12 02:00:24 +0700

    etf

--- NSW_autosar_rtw/NSW_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef RTW_HEADER_NSW_private_h_
-#define RTW_HEADER_NSW_private_h_
-#include "rtwtypes.h"
-#include "NSW_types.h"
-#endif
-
--- NSW_autosar_rtw/NSW_types.h
+++
@@ -1,4 +0,0 @@
-#ifndef RTW_HEADER_NSW_types_h_
-#define RTW_HEADER_NSW_types_h_
-#endif
-
